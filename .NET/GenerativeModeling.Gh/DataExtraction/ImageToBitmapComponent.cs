a4f1184db21ca6bfb99759e32a7b63cf3a2e6046
Sergio Guindon <sergio.guindon@gmail.com>
2020-10-18 01:46:45 -0400

    Added icons

--- .NET/GenerativeModeling.Gh/DataExtraction/ImageToBitmapComponent.cs
+++ .NET/GenerativeModeling.Gh/DataExtraction/ImageToBitmapComponent.cs
@@ -62,8 +62,8 @@ namespace GenerativeModeling.Gh.DataExtraction
             get
             {
                 //You can add image files to your project resources and access them like this:
-                // return Resources.IconForThisComponent;
-                return null;
+                return Properties.Resources.ImageToBitmap;
+
             }
         }
 
